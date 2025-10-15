82fbe26dad2cf022cde24e512d4773e6c7156cda
TINGWEIJING <tingweijingting2000@gmail.com>
2025-10-15 16:26:16 +0800

    first commit

---
+++ src/main/java/com/example/riceapi/RiceapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.riceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiceapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/riceapi/RiceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.riceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
